da676226b26a1056229f82d7a08bbcd17003e2b3
SG-mon156 <sg6625@srmist.edu.in>
2026-02-14 20:36:55 +0530

    Implemented UC1 and UC2 in OOPSBannerApp

---
+++ OOPSBannerApp.java
@@ -0,0 +1,26 @@
+/**
+ * OOPSBannerApp
+ * UC1: Prints OOPS
+ * UC2: Prints OOPS in Banner Format
+ *
+ * @author Soumya
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // UC1
+        System.out.println("OOPS");
+        System.out.println();
+
+        // UC2 - Banner
+        System.out.println(" *****    *****    *****   ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *****    *****    ***** ");
+        System.out.println("*     *  *        *        *      ");
+        System.out.println("*     *  *        *        *      ");
+        System.out.println(" *****    *        *        *      ");
+    }
+}
